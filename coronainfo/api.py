4ef7d50b27e4f3c4a9294c074202f4290fbbe899
Izzat Z <izzat.zainir11@gmail.com>
2022-09-19 12:51:16 +0800

    Refactor coronainfo module

--- coronainfo/api.py
+++ coronainfo/api.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 
+MODULE_DIR = Path(__file__).parent
+STATIC_DIR = MODULE_DIR / "static"
 
-def setup(app):
-    @app.get("/worldometer")
+
+def setup(app, path: str = "coronainfo"):
+    path = "/" + path
+
+    @app.get(path)
     async def worldometer():
         content = ""
-        path = Path(__file__).parent / "static" / "worldometer.html"
-        with open(path, "r") as file:
+        worldometer_path = STATIC_DIR / "worldometer.html"
+        with open(worldometer_path, "r") as file:
             content = file.read()
             content = " ".join(content.split())
 
